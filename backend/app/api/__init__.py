ed920a72b36075e89e436948caa2aa25e1281eb7
Julio Quiezi <julioquiezi@gmail.com>
2025-11-24 19:36:12 +0900

    feat: Add middleware for auth

---
+++ backend/app/api/__init__.py
@@ -0,0 +1,7 @@
+from fastapi import APIRouter
+from app.api.v1.supplies import routes as supplies_routes
+from app.api.v1.users import routes as users_routes
+
+api_router = APIRouter()
+api_router.include_router(users_routes.router)
+api_router.include_router(supplies_routes.router)
